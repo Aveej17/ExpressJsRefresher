c49ef5ab77fdeb55ff2cade697efc0f8e72e7894
Aveej17 <jeevaboobathi1718@gmail.com>
2024-07-31 10:48:24 +0530

    Refreshing Body-Parser middleware concepts

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log("In the middle ware")
-    next();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',(req, res, next)=>{
+    // console.log("This always runs");
+    next();    
 });
 
-app.use((req, res, next)=>{
-    console.log("In another middle ware")
-    res.send('<h1> hello to node js </h1>');
+app.use('/add-product',(req, res, next)=>{
+    // console.log("In the middle ware")
+    res.send("<form action='/product' method='POST'><input type='text' name= 'title' ><input type='number' name= 'quantity' > <button type='submit'>Submit</button></form>");
+});
+
+
+app.post('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+    
+});
+
+
+app.use('/',(req, res, next)=>{
+    // console.log("In the middle ware")
+    res.send('<h1> hello from node js </h1>');
 
 });
 
